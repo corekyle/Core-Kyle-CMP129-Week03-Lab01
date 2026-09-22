aa7a507b5e67a100935105b1db58c09671b327a9
corekyle <core.kyle@student.ccm.edu>
2026-09-22 13:58:50 -0400

    Lab 05 Problem 1 Update

--- Date.java
+++ Date.java
@@ -1,3 +1,71 @@
-public class Date {
+public class Date 
+{   // START OF Date class
+
     
-}
+    // Declare fields
+    private int month;
+    private int day;
+    private int year;
+
+    private String monthName;
+    
+    // Date setter methods
+    public void setMonth(int m)
+    {   // START OF setMonth method
+        month = m;
+    }   // END OF setMonth method
+
+    public void setDay(int d)
+    {   // START OF setDay method
+        day = d;
+    }   // END OF setDay method
+
+    public void setYear(int y)
+    {   // START OF setYear method
+        year = y;
+    }   // END OF setYear method
+
+
+    // Date getter methods
+    public int getMonth()
+    {   // START OF getMonth method
+        return month;
+    }   // END OF getMonth method
+
+    public int getDay()
+    {   // START OF getDay method
+        return day;
+    }   // END OF getDay method
+
+    public int getYear()
+    {   // START OF getYear method
+        return year;
+    }   // END OF getYear method
+
+
+    // Methods to display the date in different formats
+    public void displayDateMMDDYYYY()
+    {   // START OF displayDateMMDDYYYY method
+        System.out.printf("%d/%d/%d", month, day, year);
+    }   // END OF displayDateMMDDYYYY method
+
+    public void displayDateMonthDDYYYY()
+    {   //START OF displayDateMonthDDYYYY method
+
+        // Display date
+        System.out.printf("%s %d, %d", monthName, day, year);
+
+    }   // END OF displayDateMonthDDYYYY method
+
+    public void displayDateDDMonthYYYY()
+    {   //START OF displayDateDDMonthYYYY method
+
+
+        // Display date
+        System.out.printf("%d %s %d", day, monthName, year);
+
+    }   // END OF displayDateDDMonthYYYY method
+
+
+
+}   // END OF Date class
--- DateTest.java
+++ DateTest.java
@@ -1,3 +1,93 @@
-public class DateTest {
+import java.util.Scanner;
+
+public class DateTest 
+{   // START OF DateTest class
     
-}
+    public static void main(String[] args)
+    {   // START OF main method
+
+        // Create Scanner object
+        Scanner keyboard = new Scanner(System.in);
+
+        // Create Date object
+        Date date1 = new Date();
+
+
+        // Declare variables
+        int month;
+        int day;
+        int year;
+
+
+        // Array of months with different number of days
+        int[] month31 = {1, 3, 5, 7, 8, 10, 12};
+        int[] month30 = {4, 6, 9, 11};
+
+
+        // Get user input for month
+        System.out.print("Enter month(1-12): ");
+        month = keyboard.nextInt();
+        // Input validation for month
+        while(month < 1 || month > 12)
+        {
+            System.out.print("Invalid input. Enter month(1-12): ");
+            month = keyboard.nextInt();
+        }
+        date1.setMonth(month);
+
+        // Get user input for day
+        System.out.print("Enter day: ");
+        day = keyboard.nextInt();
+        // Input validation for day
+        // Months with 31 days
+        for(int num : month31)
+        {   // START OF for
+            if(num == month)
+            {   // START OF if
+                while(day < 1 || day > 31)
+                {   // START OF while
+                    System.out.print("Invalid input. Enter day: ");
+                    day = keyboard.nextInt();
+                }   // END OF while
+            }   // END OF if
+        }   // END OF for
+        // Months with 30 days
+        for(int num : month30)
+        {   // START OF for
+            if(num == month)
+            {   // START OF if
+                while(day <1 || day > 30)
+                {   // START OF while
+                    System.out.print("Invalid input. Enter day: ");
+                    day = keyboard.nextInt();
+                }   // END OF while
+            }   // END OF if
+        }   // END OF for
+        // February
+        if(month == 2)
+        {   // START OF if
+            while(day <1 || day > 28)
+            {
+                System.out.print("Invalid input. Enter day: ");
+                day = keyboard.nextInt();
+            }
+        }   // END OF if
+        date1.setDay(day);
+
+        // Get user input for year
+        System.out.print("Enter year: ");
+        year = keyboard.nextInt();
+        date1.setYear(year);
+
+
+        // Display date
+        System.out.println("");
+        date1.displayDateMMDDYYYY();
+        System.out.println("");
+        date1.displayDateMonthDDYYYY();
+        System.out.println("");
+        date1.displayDateDDMonthYYYY();
+        System.out.println("");
+    }   // END OF main method
+
+}   // END OF DateTest class
